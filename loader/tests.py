7418db58bf578f6301888484ab2f657daec7e90f
ndevox <nick.a.sarbicki@gmail.com>
2016-02-08 08:06:50 +0000

    Added test doc.

--- loader/tests.py
+++ loader/tests.py
@@ -2,13 +2,19 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.db.utils import IntegrityError
 from django.test import TestCase
 
 from loader.models import File, Feed
 
 class FileTestCase(TestCase):
     def setUp(self):
+        """
+        Need to set up a few base users and a feed.
+
+        The feed should only allow access for one of the two users so we can test auth rules.
+
+        :return: None
+        """
         self.good_user = User.objects.create_user('good', 'good@example.com', 'password')
         self.bad_user = User.objects.create_user('bad', 'bad@example.com', 'password')
 
@@ -18,18 +24,38 @@ class FileTestCase(TestCase):
         self.usable_feed.save()
 
     def test_needs_feed(self):
+        """
+        Ensure that a File requires a feed
+
+        :return: None
+        """
         with self.assertRaises(Feed.DoesNotExist):
             File.objects.create(user=self.good_user, file_name='test.csv')
 
     def test_needs_user(self):
+        """
+        Ensure that a File requires a user.
+
+        :return: None
+        """
         with self.assertRaises(User.DoesNotExist):
             File.objects.create(feed=self.usable_feed, file_name='test.csv')
 
     def test_needs_file_name(self):
+        """
+        Ensure a File requires a file_name.
+
+        :return: None
+        """
         with self.assertRaises(ValidationError):
             File.objects.create(feed=self.usable_feed, user=self.good_user)
 
     def test_authorised_user(self):
+        """
+        Ensure that a File can only take an authorised user for it's feed.
+
+        :return: None
+        """
         f_good = File(feed=self.usable_feed, user=self.good_user, file_name='test.csv')
         f_good.clean()
         with self.assertRaises(ValidationError):
@@ -37,6 +63,11 @@ class FileTestCase(TestCase):
             f_bad.clean()
 
     def test_created_date(self):
+        """
+        Ensure that the upload_date is the day a File is created.
+
+        :return: None
+        """
         file = File.objects.create(feed=self.usable_feed, user=self.good_user, file_name='test.csv')
         self.assertEqual(file.upload_date.date(), datetime.date.today())
 
@@ -44,6 +75,14 @@ class FileTestCase(TestCase):
         pass
 
     def test_columns(self):
+        """
+        Ensure that get columns always returns a list.
+
+        Ensure that set_columns takes a comma separated string.
+        Ensure get_columns returns the same string split into a list.
+
+        :return:
+        """
         file = File(user=self.good_user, feed=self.usable_feed, file_name='test.csv')
 
         self.assertEqual(file.get_columns(), [])

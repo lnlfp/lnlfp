fe3d33e1c74512277fb8114cc6eb3f06e466f169
ndevox <nick.a.sarbicki@gmail.com>
2016-02-09 19:48:37 +0000

    Adding in authentication

--- loader/urls.py
+++ loader/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include, url
 from loader import views
 
 urlpatterns = [
+    url(r'^login_to_app$', views.login_to_app, name='login_to_app'),
     url(r'^$', views.load_file, name='load_file'),
 ]
--- loader/views.py
+++ loader/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 from loader.forms import FileForm
 from loader.models import File
 
+def login_to_app(request):
+    """
+    Login is required to access the main pages.
+
+    The login screen passes data through to here where we can validate and then redirect.
+
+    Currently we redirect to the 'Create Session' page.
+
+    :param request: HTTP request object
+    :return: redirect
+    """
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('loader:load_file')
+    # If we have reached here, the user has not registered as logged in.
+    return redirect('django.contrib.auth.views.login')
+
 def load_file(request):
+    if not request.user.is_authenticated():
+        return redirect('django.contrib.auth.views.login')
+
     form = FileForm()
     return render(request, 'loader.html', {'form':form})

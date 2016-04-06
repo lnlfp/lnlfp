9cdcafff0cb6e1359228aaa470f62885edbe7945
Nick Sarbicki <nick.a.sarbicki@gmail.com>
2016-04-06 12:08:21 +0100

    Fixing plugin imports

--- loader/plugins/__init__.py
+++ loader/plugins/__init__.py
@@ -1,8 +1,23 @@
 import glob
+import importlib
+import inspect
 import os
 
-modules = glob.glob(os.path.dirname(__file__) + '/*.py')
+from ._interpreter import Interpreter
 
-modules = [os.path.basename(mod) for mod in modules if os.path.isfile(mod)]
 
-__all__ = [mod[:-3] for mod in modules if not mod.startswith('_')]
+def is_valid_plugin(plugin):
+    return os.path.isfile(plugin) and not os.path.basename(plugin).startswith('_')
+
+package_dir = 'loader.plugins'
+
+packages_to_import = [package for package in glob.glob(os.path.dirname(__file__) + '/*.py') if is_valid_plugin(package)]
+
+packages = [importlib.import_module('.' + os.path.basename(package)[:-3], package_dir) for package in packages_to_import]
+
+plugins = []
+
+for package in packages:
+    for value in package.__dict__.values():
+        if inspect.isclass(value) and issubclass(value, Interpreter) and value is not Interpreter:
+            plugins.append(value)
--- loader/plugins/python_interpreter.py
+++ loader/plugins/python_interpreter.py
@@ -6,7 +6,6 @@ from ._interpreter import Interpreter
 class PythonInterpreter(Interpreter):
     LANGUAGE = 'Python'
     EXTENSION = '.py'
-    META = {}
 
     def run(self, file, *args, **kwargs):
         process = ['python', file.data.name] + args

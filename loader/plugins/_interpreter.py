25b006a724eb77abe800c99fda039a0e07926970
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-04-11 19:40:13 +0100

    Lets use abc

--- loader/plugins/_interpreter.py
+++ loader/plugins/_interpreter.py
@@ -1,9 +1,13 @@
+import abc
+
 class Interpreter:
     """
     Base interpreter class.
 
     All plugins need to inherit this class to be recognised.
     """
+    __metaclass__ = abc.ABCMeta
+
     LANGUAGE = None  # What language is this for?
     EXTENSION = None  # What is the standard file extension for programs of this language?
     META = {}  # What extra data is required to run this?
@@ -12,13 +16,12 @@ class Interpreter:
         if not (self.LANGUAGE and self.EXTENSION):
             raise NotImplementedError('Class {} lacks the required properties.'.format(self.__class__.__name__))
 
+    @abc.abstractmethod
     def run(self, file, *args, **kwargs):
         """
         Method used to run procedures.
 
-        Raise an error here to make sure it gets overwritten.
-
         :param file: str, filepath to the procedure to run.
         :param args: list of arguments to attach to the procedure.
         """
-        raise NotImplementedError('Class {} lacks a run method.'.format(self.__class__.__name__))
+        pass

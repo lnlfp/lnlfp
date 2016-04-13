79330c88362314ac74607cb58168b9ed6a51be27
tedjohansson <uehkz7uehkz7>
2016-04-13 19:09:18 +0100

    A simple oracle plugin

---
+++ loader/plugins/oracle_interpreter.py
@@ -0,0 +1,35 @@
+import subprocess
+
+from ._interpreter import Interpreter
+
+
+class OracleInterpreter(Interpreter):
+    """
+    Basic interpreter for Oracle.
+    """
+    LANGUAGE = 'oracle'
+    EXTENSION = '.sql'
+
+    @staticmethod
+    def run(proc, *args):
+        """
+        Run a python script with the given args.
+
+        :param proc: Procedure obj, the procedure we are running.
+        :param args: list of arguments to pass to the command line.
+        :return: str, stdoutput from the process.
+        """
+
+        process = ['sqlplus inbound/*@s1 @', proc.procedure.name] + list(args)
+
+        running = subprocess.Popen(process, stdout=subprocess.PIPE)
+
+        output = []
+
+        while True:  # While this is running give out the output.
+            outp = running.stdout.readline()
+            if not outp:
+                break
+            output.append(outp)
+
+        return output

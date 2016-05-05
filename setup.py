e42f3d0a6b2f66232961ae2f30c2b955ee98289d
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-05-05 11:14:37 +0100

    Making requirements looser and sorting out setup.py. Pandas kills it :(

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ import os
 import random
 import string
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from pip.req import parse_requirements
+
 
 def main():
     """
@@ -9,12 +15,27 @@ def main():
 
     This involves creating a secret key, setting up groups and permissions.
     """
+
     template = os.path.join('lionel', 'example_settings.py')
     settings = os.path.join('lionel', 'settings.py')
 
     with open(template, 'r') as template_file, open(settings, 'w') as settings_file:
         settings_file.write(template_file.read().replace("SECRET_KEY = 'Enter your secret key here'",
                                                          "SECRET_KEY = '{}'".format(make_key())))
+
+    package_setup()
+
+
+def package_setup():
+
+    reqs = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+
+    setup(name='lionel',
+          version='0.1',
+          description='Automated data wrangling with Django',
+          author='Nick Sarbicki',
+          author_email='nick.a.sarbicki@gmail.com',
+          install_requires=reqs)
 
 
 def make_key(pk=None):
